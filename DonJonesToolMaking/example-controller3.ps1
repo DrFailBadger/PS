54a45ab4997bfd2111aacbe03110addb25fbafdb
DrFailBadger <David.Hislop@live.co.uk>
2017-01-06 21:56:04 +0000

    Remove as already in folder

--- DonJonesToolMaking/example-controller3.ps1
+++
@@ -1,8 +0,0 @@
-﻿param(
-    [string[]]$ComputerName
-)
-
-Get-DriveInfo -ComputerName $ComputerName |
-Select-Object -Property ComputerName,DriveLetter,DriveKind,FreeSpace,Capacity,@{name='Free%';expression={$_.FreeSpace / $_.Capacity * 100 -as [int]}} |
-ConvertTo-HTML -PreContent "<h1>Drive Info Report</h1>" |
-Out-File driveinforeport.html
