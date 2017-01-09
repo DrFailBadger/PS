61e8eeb83a678890346dfcf201aa400accd60101
DrFailBadger <David.Hislop@live.co.uk>
2017-01-09 09:08:04 +0000

    Add new funtion from the PS nuggets
    Add new funtion from the PS nuggets

---
+++ Functions/Get-SysInfo.ps1
@@ -0,0 +1,23 @@
+﻿Function get-SysInfo {
+    Param(
+        [String[]]$ComputerName
+    )
+
+    foreach ($comp in $ComputerName) {
+         $os = Get-WmiObject -Class Win32_operatingsystem -ComputerName $Comp
+         $CS = Get-WmiObject -Class Win32_computersystem -ComputerName $comp
+         $bios = Get-WmiObject -Class Win32_BIOS -ComputerName $comp
+    
+
+        $props = [ordered]@{'ComputerName' =$Comp
+                            'OSversion' = $os.version
+                            'SPVersion' = $os.servicepackmajorversion
+                            'MFG' = $cs.managacturer
+                            'Model' = $cs.model
+                            'RAM'= $cs.totalphysicalmemory
+                            'BISOSerial' = $bios.serialnumber
+                }
+        $obj = New-Object -TypeName psobject -Property $props
+        Write-Output $obj
+    }
+}
