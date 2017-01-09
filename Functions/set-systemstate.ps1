6a99f8a5374d821c807699d4d0634c0e757c9908
DrFailBadger <David.Hislop@live.co.uk>
2017-01-09 14:43:33 +0000

    adding function set-state ps learning

---
+++ Functions/set-systemstate.ps1
@@ -0,0 +1,38 @@
+﻿
+function set-computerState {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true)]
+        [String[]]$computername,
+
+        [Switch]$Force,
+
+        [Parameter(ParameterSetName='Logoff')]
+        [Switch]$Logoff,
+
+        [Parameter(ParameterSetName='Restart')]
+        [Switch]$Restart,
+
+        [Parameter(ParameterSetName='Shutdown')]
+        [Switch]$Shutdown,
+
+        [Parameter(ParameterSetName='Poweroff')]
+        [Switch]$Poweroff
+    )
+
+    Process {
+        foreach ($comptuter in $computername){
+            $os =Get-WmiObject -ComputerName $computer -Class Win32_OperatingSystem
+            if ($Logoff) { $arg = 0 }
+            if ($Restart) { $arg = 2 }
+            if ($Shutdown) { $arg = 1 }
+            if ($Poweroff) { $arg = 8 }
+            if ($Force) { $Arg += 4 }
+            $os.Win32Shutdown($Arg)
+        }
+    }
+
+
+}
