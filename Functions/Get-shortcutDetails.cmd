b2e3229d15352c21b11561ceced2bbd375b469f5
DrFailBadger <David.Hislop@live.co.uk>
2020-04-26 20:30:06 +0100

    m,m#

---
+++ Functions/Get-shortcutDetails.cmd
@@ -0,0 +1,2 @@
+C:\windows\system32\WindowsPowershell\v1.0\Powershell.exe -ExecutionPolicy Bypass -File %~dp0Get-shortcutDetails.ps1 -loglocation \\fs01\wip\david\logs\shortcutdetails\
+pause
--- Functions/Install.cmd
+++ Functions/Install.cmd
@@ -1,1 +1,2 @@
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -file "c:\packages\Expand.ps1"
+Pause
